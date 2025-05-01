f11631ca9ddd2565a4a2282ba528a0846a1b783d
Takuya Yamakoshi <takyamakoshi@puc.pu-toyama.ac.jp>
2025-05-02 03:44:16 +0900

    feat: 自動生成コードを追加

--- go.mod
+++ go.mod
@@ -3,13 +3,21 @@ module github.com/t-yamakoshi/go-grpc-sample
 go 1.24.0
 
 require (
+	github.com/caarlos0/env/v11 v11.3.1
+	github.com/joho/godotenv v1.5.1
+	google.golang.org/grpc v1.72.0
+	google.golang.org/protobuf v1.36.6
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gen v0.3.27
+	gorm.io/gorm v1.26.0
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/caarlos0/env/v11 v11.3.1 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/go-sql-driver/mysql v1.9.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
@@ -17,12 +25,7 @@ require (
 	golang.org/x/text v0.24.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	google.golang.org/grpc v1.72.0 // indirect
-	google.golang.org/protobuf v1.36.6 // indirect
 	gorm.io/datatypes v1.2.4 // indirect
-	gorm.io/driver/mysql v1.5.7 // indirect
-	gorm.io/gen v0.3.27 // indirect
-	gorm.io/gorm v1.26.0 // indirect
 	gorm.io/hints v1.1.0 // indirect
-	gorm.io/plugin/dbresolver v1.5.0 // indirect
+	gorm.io/plugin/dbresolver v1.6.0 // indirect
 )
--- tools/mysqlgen/main.go
+++ tools/mysqlgen/main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	schema "github.com/t-yamakoshi/go-grpc-sample/schema/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gen"
 	"gorm.io/gorm"
-
-	schema "github.com/t-yamakoshi/go-grpc-sample/schema/mysql"
 )
 
-//go generate go run main.go
-
 func main() {
+	schemas := []any{
+		&schema.Todo{},
+	}
 	g := gen.NewGenerator(
 		gen.Config{
-			OutPath:           "../pkg/adapter/mysqlgen",
+			OutPath:           "pkg/adapter/mysqlgen",
 			Mode:              gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
 			FieldWithIndexTag: true,
 			FieldWithTypeTag:  true,
@@ -24,39 +23,17 @@ func main() {
 		},
 	)
 
-	config := mysql.Open("mysql:mysqlroot@tcp(127.0.0.1:3306)/tododb?charset=utf8mb4&parseTime=True&loc=Local")
-	db, err := gorm.Open(config)
+	director := mysql.Open("root:@tcp(127.0.0.1:3306)/tododb?charset=utf8mb4&parseTime=True&loc=Local")
+	db, err := gorm.Open(director)
 	if err != nil {
 		log.Fatal("failed to connect to database:", err)
 	}
 
-	mysqlDb, err := db.DB()
-	if err != nil {
-		log.Fatal("failed to get database:", err)
-	}
-	defer mysqlDb.Close()
+	g.UseDB(db)
+	db.AutoMigrate(schemas...)
 
-	stmt := &gorm.Statement{DB: db}
-	if err = stmt.Parse(&schema.Todo{}); err != nil {
-		log.Fatal("failed to parse statement:", err)
-	}
-	fmt.Println(stmt.Schema.CreateTableSQL())
+	g.ApplyBasic(schemas...)
+	g.ApplyInterface(func() {}, schemas...)
 
-	for _, field := range stmt.Schema.Fields {
-		for _, tag := range field.Tag {
-			if tag.Name == "gorm" {
-				value, ok := tag.Options["uniqueIndex"]
-				if ok {
-					if ok {
-						fmt.Printf("CREATE UNIQUE INDEX idx_%s_%s ON %s(%s);\n", stmt.Schema.Table, field.DBName, stmt.Schema.Table, field.DBName)
-					}
-				}
-			}
-		}
-
-		err = db.AutoMigrate(&schema.Todo{})
-		if err != nil {
-			log.Fatal("failed to migrate database:", err)
-		}
-	}
+	g.Execute()
 }
